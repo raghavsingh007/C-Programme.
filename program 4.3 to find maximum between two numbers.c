c4ef5bbec354cae34e603373e8ec0a1bf7e94cbb
Raghav Singh <145863192+raghavsingh007@users.noreply.github.com>
2023-11-19 18:24:56 +0530

    Add files via upload

---
+++ program 4.3 to find maximum between two numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+ 
+int main() { 
+  int num1, num2, max; 
+ 
+  printf("Enter two numbers: "); 
+  scanf("%d %d", &num1, &num2); 
+ 
+  if (num1 > num2) { 
+    max = num1; 
+  } else { 
+    max = num2; 
+  } 
+ 
+  printf("The maximum number is %d\n", max); 
+ 
+  return 0; 
+} 
