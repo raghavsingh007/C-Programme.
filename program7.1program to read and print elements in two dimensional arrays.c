5575a22a6118580eae2097ff0341fa7b5745cf3a
Raghav Singh <145863192+raghavsingh007@users.noreply.github.com>
2023-11-24 22:12:28 +0530

    Add files via upload

---
+++ program7.1program to read and print elements in two dimensional arrays.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h> 
+int main() 
+{ 
+
+int marks[3][3]; int r,c;  
+for (r = 0; r < 3; r++) { 
+ for(c = 0; c < 3; c++) { 
+ printf("\nENTER VALUE AT marks[%d][%d] : ", r, c); 
+scanf("%d", &marks[r][c]); 
+ } 
+ } 
+
+printf("\n2D Array Values are: \n\n"); 
+for (r = 0; r < 3; r++) { 
+ printf("\nROW %d : ",r); 
+for(c = 0; c < 3; c++) {
+printf("%d ",marks[r][c]); 
+ } 
+printf("\n---------------------------"); 
+} 
+return 0;
+}
